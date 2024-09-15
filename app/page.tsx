e0201ca64f5adee96ac565e106f8c6937cf9a38d
cornchiplick <ilsunkiss@gmail.com>
2024-09-15 15:59:12 +0900

    Fix: 조건부 modifier 인 invalid 사용 - focus modifier와 중첩해서 사용하는 예시

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,25 @@ export default function Home() {
     <main
       className="bg-gray-100 sm:bg-red-100 
       md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100
-       h-screen flex
-      items-center justify-center p-5"
+      h-screen flex items-center justify-center p-5"
     >
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
         <input
           className="w-full rounded-full h-10
             bg-gray-200 pl-5 outline-none ring
-            ring-transparent focus:ring-orange-500
+            ring-transparent focus:ring-green-500
             focus:ring-offset-2 transition-shadow
-            placeholder:text-red-600, 
+            placeholder:drop-shadow invalid:focus:ring-red-100
             "
-          type="text"
+          type="email"
+          required
           placeholder="Search here..."
         />
         <button
           className="text-white py-2
           rounded-full active:scale-90
           transition-transform font-medium
-          outline-none md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400
+          outline-none md:px-10 bg-black
           "
         >
           Search
