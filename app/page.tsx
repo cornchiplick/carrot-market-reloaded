777cfa3f46e6a571ef72e4219ca57978d3e2941a
cornchiplick <ilsunkiss@gmail.com>
2024-09-15 16:33:26 +0900

    Feat: 새 컴포넌트 init

--- app/page.tsx
+++ app/page.tsx
@@ -6,34 +6,23 @@ export default function Home() {
       h-screen flex items-center justify-center p-5"
     >
       <div
-        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex
-        flex-col md:flex-row gap-2 *:outline-none
-        has-[:invalid]:ring-red-100
-        ring ring-transparent transition-shadow"
+        className="bg-white shadow-lg p-5
+        rounded-3xl w-full max-w-screen-sm flex
+        flex-col gap-4"
       >
-        <input
-          className="w-full rounded-full h-10
-            bg-gray-200 pl-5 ring
-            ring-transparent focus:ring-green-500
-            focus:ring-offset-2 transition-shadow
-            placeholder:drop-shadow invalid:focus:ring-red-500 peer
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 
             "
-          type="text"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required.
-        </span>
-        <button
-          className="text-white py-2
-          rounded-full active:scale-90
-          transition-transform font-medium
-          md:px-10 bg-black 
-          "
-        >
-          Log in
-        </button>
+          >
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
