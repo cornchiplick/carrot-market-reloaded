695865951b35099e9cdc3c4a8f41cdc37f643a2a
cornchiplick <ilsunkiss@gmail.com>
2024-09-15 16:14:01 +0900

    Fix: 컨테이너에 적용하는 클래스 - *를 하위 자식요소에 모두 적용되는 스타일을 추가할 수 있다.

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,18 @@ export default function Home() {
       md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100
       h-screen flex items-center justify-center p-5"
     >
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div
+        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2
+      *:outline-none"
+      >
         <input
           className="w-full rounded-full h-10
-            bg-gray-200 pl-5 outline-none ring
+            bg-gray-200 pl-5 ring
             ring-transparent focus:ring-green-500
             focus:ring-offset-2 transition-shadow
             placeholder:drop-shadow invalid:focus:ring-red-500 peer
             "
-          type="email"
+          type="text"
           required
           placeholder="Email address"
         />
@@ -24,7 +27,7 @@ export default function Home() {
           className="text-white py-2
           rounded-full active:scale-90
           transition-transform font-medium
-          outline-none md:px-10 bg-black 
+          md:px-10 bg-black 
           "
         >
           Log in
