381d99b8bdd37669e43eb34b44f759e199c1ed69
cornchiplick <ilsunkiss@gmail.com>
2024-09-16 21:08:10 +0900

    Chore: prettier의 tailwind 플러그인 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 export default function Home() {
   return (
-    <main
-      className="bg-gray-100 sm:bg-red-100 
-      md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100
-      h-screen flex items-center justify-center p-5">
-      <div
-        className="bg-white shadow-lg p-5
-      rounded-3xl w-full max-w-screen-sm flex
-      flex-col gap-4">
+    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+      <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
         <input />
         <a href="#">aaaa</a>
         <button className="btn">Submit</button>
