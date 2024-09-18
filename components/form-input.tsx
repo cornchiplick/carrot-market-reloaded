3a6e46d47b3b6ac0eb121985364176c712db1537
cornchiplick <ilsunkiss@gmail.com>
2024-09-18 18:03:40 +0900

    Fix: use server 를 활용한 post 요청

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,12 +3,14 @@ interface FomInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
-export default function FormInput({type, placeholder, required, errors}: FomInputProps) {
+export default function FormInput({type, placeholder, required, errors, name}: FomInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
+        name={name}
         className="h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500"
         type={type}
         placeholder={placeholder}
