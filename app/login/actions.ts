122a74c18677493dfaad71dc51f493288d75fd44
cornchiplick <ilsunkiss@gmail.com>
2024-09-19 21:36:47 +0900

    Fix: ts 에러 메세지를 막기 위한 방편

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,7 +1,8 @@
 "use server";
 
-export const handleForm = async (prevState: any, formData: FormData) => {
+export const handleForm = async (prevState: unknown, formData: FormData) => {
   console.log(prevState);
+  console.log(formData);
   await new Promise((resolve) => setTimeout(resolve, 5000));
   return {
     errors: ["wrong password", "password too short"],
