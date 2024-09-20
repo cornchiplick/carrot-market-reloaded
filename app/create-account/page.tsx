a055650dd51ac1da34ca25b8ae66ccacf2e4db1e
cornchiplick <ilsunkiss@gmail.com>
2024-09-20 21:14:39 +0900

    Feat: 계정생성 시 함수 추가 및 zod를 사용한 검증 일부

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import {useFormState} from "react-dom";
+import {createAccount} from "./actions";
 
 export default function CreateAccount() {
+  const [state, dispatch] = useFormState(createAccount, null);
+
   return (
     <div className="flex flex-col gap-10 px-5 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={dispatch} className="flex flex-col gap-3">
         <FormInput name="username" type="text" placeholder="Username" required />
         <FormInput name="email" type="email" placeholder="Eamil" required />
         <FormInput name="password" type="password" placeholder="Password" required />
