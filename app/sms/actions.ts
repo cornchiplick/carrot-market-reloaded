6183e2211ce372ad38fb0929a8179ff640d35c26
cornchiplick <ilsunkiss@gmail.com>
2024-09-27 01:04:15 +0900

    Feat: zod와 validator 를 이용한 smsLogIn 함수 구현 및 에러처리

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,8 +1,48 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {
-  const data = {
-    code: formData.get("code"),
-  };
-  console.log(data);
+import {redirect} from "next/navigation";
+import validator from "validator";
+import {z} from "zod";
+
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine((phone) => validator.isMobilePhone(phone, "ko-KR"), "Wrong phone format");
+
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogIn(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      console.log(result.error.flatten());
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      // login 로직
+
+      // home으로 redirect
+      redirect("/");
+    }
+  }
 }
