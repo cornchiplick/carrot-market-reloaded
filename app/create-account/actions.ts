e28a1b7593674f3b650d2102f78443995be48bdc
cornchiplick <ilsunkiss@gmail.com>
2024-09-30 22:42:11 +0900

    Feat: bcrypt 를 이용하여 입력받은 비밀번호를 해싱처리하고 사용자 생성

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import {PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR} from "@/lib/constants";
 import db from "@/lib/db";
+import bcrypt from "bcrypt";
 import {z} from "zod";
 
 const checkUsername = (username: string) => !username.includes("potato");
@@ -75,7 +76,22 @@ export async function createAccount(prevState: any, formData: FormData) {
     return result.error.flatten();
   } else {
     // hash password
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
     // save the user to db
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    console.log("user", user);
+
     // log the user in
     // redirect "/home"
   }
