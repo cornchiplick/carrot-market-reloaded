feb889a801b9d3d292694de06716f3732d6563a1
cornchiplick <ilsunkiss@gmail.com>
2024-10-03 18:55:59 +0900

    Fix: middleware가 실행되는 경로조건에 대해 config 오브젝트에 작성 - 반드시 변수명이 config일 것 - matcher에 대응하는 경로일 경우에만 middleware가 실행된다 - matcher에 정규식을 작성해도 된다

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import {NextRequest} from "next/server";
-import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
