1a34a24b46bfee88b27dbddf79d42ed4be4800bc
cornchiplick <ilsunkiss@gmail.com>
2024-10-06 21:58:31 +0900

    Refact: 긴 fetch 문을 캡슐화하기

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,6 +1,7 @@
 import {sessionLogin} from "@/app/login/actions";
 import db from "@/lib/db";
 import {NextRequest} from "next/server";
+import {getAccessToken, getGithubEmail, getGithubProfile} from "./actions";
 
 export async function GET(request: NextRequest) {
   // github로부터 redirect된 url에는 code라는 query parameter가 존재
@@ -10,51 +11,15 @@ export async function GET(request: NextRequest) {
       status: 400,
     });
   }
+  const access_token = await getAccessToken(code);
 
-  // code를 이용해 access token을 받아옴
-  const accessTokenParams = new URLSearchParams({
-    client_id: process.env.GITHUB_CLIENT_ID!,
-    client_secret: process.env.GITHUB_CLIENT_SECRET!,
-    code,
-  }).toString();
-  const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
-  const accessTokenResponse = await fetch(accessTokenURL, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  // access token을 이용해 user email을 받아옴.
+  const userEmail = await getGithubEmail(access_token);
+  console.log("github Email : ", userEmail);
 
   // access token을 이용해 user profile을 받아옴.
-  // user profile에는 github id, avatar url, login 정보가 있음
-  const {error, access_token} = await accessTokenResponse.json();
-  if (error) {
-    return new Response(null, {
-      status: 400,
-    });
-  }
+  const {id, avatar_url, login} = await getGithubProfile(access_token);
 
-  // email 가져와보기
-  const userEmailResponse = await fetch("https://api.github.com/user/emails", {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-    cache: "no-cache",
-  });
-
-  const emails = await userEmailResponse.json();
-  const userEmail = emails.find((email: {primary: boolean}) => email.primary)?.email;
-
-  // github id를 이용해 user를 찾음
-  const userProfileResponse = await fetch("https://api.github.com/user", {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-    cache: "no-cache",
-  });
-
-  // user가 존재하면 session에 user id를 저장하고 profile 페이지로 redirect (이후 email/pwd 방식 로그인과 동일)
-  const {id, avatar_url, login} = await userProfileResponse.json();
   const user = await db.user.findUnique({
     where: {
       github_id: id + "",
