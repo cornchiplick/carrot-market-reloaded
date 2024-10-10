316e63b11f92c45b7ca8a7a1f7d9d61cff0e9443
cornchiplick <ilsunkiss@gmail.com>
2024-10-10 22:26:19 +0900

    Fix: 코드챌린지 해보기 - 유저가 이미지를 업로드했는지 확인하기 - 이미지 사이즈가 4MB 이하인지 확인하기

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import {LIMIT_FILE_SIZE} from "@/lib/constants";
+
 export async function uploadProduct(formData: FormData) {
   const data = {
     photo: formData.get("photo"),
@@ -7,5 +9,18 @@ export async function uploadProduct(formData: FormData) {
     price: formData.get("price"),
     description: formData.get("description"),
   };
+
+  if (data.photo && data.photo instanceof File) {
+    if (!data.photo.type.startsWith("image/")) {
+      return;
+    }
+
+    if (data.photo.size > LIMIT_FILE_SIZE) {
+      return;
+    }
+  } else {
+    return;
+  }
+
   console.log(data);
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,4 @@ export const PASSWORD_MIN_LENGTH = 4;
 export const PASSWORD_REGEX = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/);
 export const PASSWORD_REGEX_ERROR =
   "A password must have lowercase, UPPERCASE, a numnber and special characters";
+export const LIMIT_FILE_SIZE = 1024 * 1024 * 4; // 4MB
