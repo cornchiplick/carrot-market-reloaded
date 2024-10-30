bf46c8c00277d5cf50290f215f8f165f7b60469b
cornchiplick <ilsunkiss@gmail.com>
2024-10-30 21:59:54 +0900

    Fix: useOptimistic hook을 이용해서 낙관적 업데이트 하기

---
+++ app/posts/[id]/actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import {revalidateTag} from "next/cache";
+
+export const likePost = async (postId: number) => {
+  const session = await getSession();
+  try {
+    await db.like.create({
+      data: {
+        postId: postId,
+        userId: session.id!,
+      },
+    });
+    revalidateTag(`like-status-${postId}`);
+  } catch (e) {}
+};
+
+export const dislikePost = async (postId: number) => {
+  const session = await getSession();
+  try {
+    await db.like.delete({
+      where: {
+        id: {
+          postId: postId,
+          userId: session.id!,
+        },
+      },
+    });
+    revalidateTag(`like-status-${postId}`);
+  } catch (e) {}
+};
