1733b2ac6a426424da1dcf165e1b4bf25cd09642
cornchiplick <ilsunkiss@gmail.com>
2024-11-08 22:57:04 +0900

    Feat: 실시간 채팅룸을 위한 supabase 연결중

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,10 @@
 import db from "@/lib/db";
+import getSession from "@/lib/session";
 import {formatToWon} from "@/lib/utils";
 import {UserIcon} from "@heroicons/react/24/solid";
 import {unstable_cache as nextCache, revalidateTag} from "next/cache";
 import Image from "next/image";
-import Link from "next/link";
-import {notFound} from "next/navigation";
+import {notFound, redirect} from "next/navigation";
 
 async function getIsOwner(userId: number) {
   // const session = await getSession();
@@ -75,6 +75,29 @@ export default async function ProductDetail({params}: {params: {id: string}}) {
     revalidateTag("product-title");
   };
 
+  const createChatRoom = async () => {
+    "use server";
+    const session = await getSession();
+    const room = await db.chatRoom.create({
+      data: {
+        users: {
+          connect: [
+            {
+              id: product.userId,
+            },
+            {
+              id: session.id,
+            },
+          ],
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    redirect(`/chats/${room.id}`);
+  };
+
   return (
     <div>
       <div className="relative aspect-square">
@@ -111,9 +134,11 @@ export default async function ProductDetail({params}: {params: {id: string}}) {
             Delete product
           </button>
         ) : null}
-        <Link className="rounded-md bg-orange-500 px-5 py-2.5 font-semibold text-white" href={``}>
-          채팅하기
-        </Link>
+        <form action={createChatRoom}>
+          <button className="rounded-md bg-orange-500 px-5 py-2.5 font-semibold text-white">
+            채팅하기
+          </button>
+        </form>
       </div>
     </div>
   );
